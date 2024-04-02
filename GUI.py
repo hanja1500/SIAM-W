819419c38335441d8817e94a740da08b224e3e22
Min Yewon <88868805+alsspp01@users.noreply.github.com>
2024-04-02 21:32:59 +0900

    Create GUI.py

---
+++ GUI.py
@@ -0,0 +1,69 @@
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QLabel,
+    QLineEdit,
+    QPushButton,
+    QListWidget,
+    QHBoxLayout,
+    QVBoxLayout,
+    QWidget,
+)
+import sys
+
+SQLi = ['vulnerabilities']
+manual = ""
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("SIAM-W")
+
+        layout = QVBoxLayout()
+
+        notify = QLabel("Enter your URL to receive manual")
+        layout.addWidget(notify)
+        
+        input_layout = QHBoxLayout()
+        layout.addLayout(input_layout)
+
+        self.lineedit = QLineEdit()
+        self.lineedit.setPlaceholderText("Enter your URL")
+        input_layout.addWidget(self.lineedit)
+
+        button = QPushButton("Enter")
+        button.setFixedSize(QSize(100, 30))
+        button.clicked.connect(self.execute)
+        input_layout.addWidget(button)
+        
+        self.vuln = QListWidget()
+        self.vuln.addItems(SQLi)
+        self.vuln.currentItemChanged.connect(self.print_manual)
+        layout.addWidget(self.vuln)
+
+        self.man = QLabel(manual)
+        layout.addWidget(self.man)
+
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+
+        self.setCentralWidget(central_widget)
+
+    def execute(self):
+        url = self.lineedit.text()
+        SQLi.pop()
+        SQLi.append('UNION-based') # 여기에 SQLi 종류 목
+        self.vuln.takeItem(0)
+        self.vuln.addItems(SQLi)
+        
+    def print_manual(self):
+        vulnerable = self.vuln.currentItem().text()
+        manual = "change your brain" # 여기에 vulnerable에 맞 매뉴얼
+        self.man.setText(manual)
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
