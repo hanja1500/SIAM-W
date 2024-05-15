7f18b11bf54e93b928402a4cf72dc7b5bfd72975
unknown <hanja1500@naver.com>
2024-05-15 20:20:34 +0900

    modify test.php

--- test.php
+++ test.php
@@ -1,1 +1,24 @@
-<?php phpinfo(); ?>
+<?php
+    session_start();
+    $username = $_GET['id'];
+    $userpass = $_GET['pw'];
+
+    $connect = new PDO ('mysql:host=localhost; dbname=serverDB', 'root', '1234');
+
+    $sql = $connect->query("SELECT * from member WHERE ID='$username'");
+    $result = $sql->fetch();
+
+    if( !$result ) {
+        echo "<script>alert('Invalid username or password')</script>";
+        echo "<script>location.replace('login.php')</script>";
+        exit;
+    }
+    else if( $result == null ) {
+        echo "<script>alert('Input the ID and password!')</script>";
+        echo "<script>location.replace('login.php')</script>";
+        exit;
+    }
+
+    $_SESSION['username'] = $username;
+    echo "<script>location.replace('list.php')</script>";
+?>
