62bcaf754d65c0b22bf080cf63d648174723ed7a
unknown <hanja1500@naver.com>
2024-05-28 20:40:51 +0900

    modified SaveLine_Extraction.py

--- SaveLine_Extraction.py
+++ SaveLine_Extraction.py
@@ -1,16 +1,3 @@
-"""
-SaveLine_Extraction.py
-
-1. 입력값 검증 강화
-2. 프리페이드 스테이트
-3. 에러 메세지 정보
-
-4. 서버 보안 강화
-5. 비동기 처리 / 캐싱
-6. 최소 권한 부여
-7. 탐색용 DB 분리
-8. 데이터 암호화
-"""
 import os
 import pandas as pd
 import platform
@@ -18,6 +5,7 @@ import platform
 # 필요한 조건을 포함하는 라인 추출
 relevant_lines = []
 change_lines = []
+
 
 def process_file(file_path):
     global relevant_lines
@@ -42,8 +30,9 @@ def process_file(file_path):
 
     checking()
     minimize_error_message()
-    #popup()
-    save_csv()
+    popup()
+    if change_lines:
+        save_csv()
 
 
 # 입력값 검증 강화 및 Prepared statment 작성
@@ -64,7 +53,10 @@ def checking():
             lower = iline[1].lower()
             if "id" in lower:
                 if "pw" in lower:
-                    change_lines.insert(ii,
+                    if "and" in lower:
+                        continue
+                    else:
+                        change_lines.insert(ii,
                                         (iline[0],
                                          "$statement = $connect->prepare('SELECT * FROM member WHERE ID = :id AND PW = :pw');\n"
                                          "$statement->bindValue(':id', $username, PDO::PARAM_STR);\n"
@@ -78,11 +70,14 @@ def checking():
         for (ti, tline) in enumerate(relevant_lines):
             lower = tline[1].lower()
             if "select" in lower:
-                temp = change_lines.pop()
-                change_lines.insert(ti, (tline[0], "$match = '/^[a-zA-z0-9]+$/';\n\n"
+                if change_lines:
+                    temp = change_lines.pop()
+                    change_lines.insert(ti, (tline[0], "$match = '/^[a-zA-z0-9]+$/';\n\n"
                                                    "if(!preg_match($match, $<variable>)){\n"
                                                    "echo '<script>alert('use only alphabet and numbers')</script>';\n"
                                                    "exit;}\n\n" + temp[1]))
+                else:
+                    continue
             else:
                 continue
 
@@ -171,9 +166,24 @@ def minimize_error_message():
 
 # 기타 출력
 def popup():
-    popup()
+    global change_lines
+
+    if change_lines:
+        change_lines.insert(0, (0, "[*] <Additional Manual>\n\n"
+                                     "[*] [Possibility] Reinforce the Server Security\n"
+                                     "[*] [Manual] Use Frameworks Uploaded on Internet.\n"
+                                     "[*] [Manual] You Can Use Web Cache & More Security Functions.\n\n"
+                                     "[*] [Possibility] Use Another SQL Databases\n"
+                                     "[*] [Manual] Split the Registering Database and Searching Database.\n\n"
+                                     "[*] [Possibility] Set the Previlage to Least\n"
+                                     "[*] [Manual] Split the Managing Account From Unknown Access User.\n\n"
+                                     "[*] [Possibility] Unsecure Network Connection\n"
+                                     "[*] [Manual] Use SSL/TLS Network Connection.\n"))
+    else:
+        return
 
 
+# csv 파일 출력
 def save_csv():
     global change_lines
 
@@ -185,16 +195,10 @@ def save_csv():
 
 
 """
-
-# 프리페어드: 입력값 강화의 일부로 정규 표현식을 사용하여 입력값을 검증하고, Prepared Statement를 사용하여 쿼리를 작성
-def execute_query(conn, username):
-    if validate_input(username):
-        try:
-            with conn.cursor() as cursor:
-                sql = "SELECT * FROM users WHERE username = %s"
-                cursor.execute(sql, (username,))
-                result = cursor.fetchone()
-                print(result)
-        except Exception as e:
-            print("쿼리 실행 중 오류가 발생했습니다.")
+SaveLine_Extraction.py
+4. 서버 보안 강화
+5. 비동기 처리 / 캐싱
+6. 최소 권한 부여
+7. 탐색용 DB 분리
+8. 데이터 암호화
 """
